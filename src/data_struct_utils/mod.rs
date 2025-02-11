5f4c0861f8a3bc02825a53bb6c0e83f88eb20142
Corentin <c.vaillant848@gmail.com>
2025-02-11 22:22:13 +0100

    commit before midnight

--- src/data_struct_utils/mod.rs
+++ src/data_struct_utils/mod.rs
@@ -1,20 +1,3 @@
 pub mod kd_tree;
 pub mod kd_tree_traits;
 
-// #[cfg(test)]
-// mod tests {
-//     use crate::data_struct_utils::kd_tree::{KdTree, Point};
-
-//     #[test]
-//     fn test_nearest_neighbor() {
-//         let points = vec![
-//             Point { position: [0.0, 0.0] },
-//             Point { position: [1.0, 1.0] },
-//             Point { position: [2.0, 2.0] },
-//         ];
-//         let tree = KdTree::from(points);
-//         let target = Point { position: [1.1, 1.1] };
-//         let nearest = tree.nearest(target).unwrap();
-//         assert_eq!(nearest.position, [1.0, 1.0]);
-//     }
-// }
